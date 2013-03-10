1253baabc76def66389f884a941b02009f2d5a7b
Rehana Khan <rehanakhan00@gmail.com>
2013-03-10 18:19:52 -0400

    Chapter 5 Exercise 1

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,12 +1,32 @@
-require 'spec_helper'
-
-describe StaticPagesController do
-
-  describe "GET '...'" do
-    it "returns http success" do
+#require 'spec_helper'
+#describe StaticPagesController do
+#  describe "GET '...'" do
+#    it "returns http success" do
      #: get '...'
-      response.should be_success
-    end
-  end
+#      response.should be_success
+ #   end
+  
+#end
+#end
 
+
+
+describe "Static pages" do
+
+subject { page }
+
+shared_examples_for "all static pages" do
+  it { should have_selector('h1',    text: heading) }
+  it { should have_selector('title', text: full_title(page_title)) }
 end
+
+describe "Home page" do
+  before { visit root_path }
+  let(:heading)    { 'Sample App' }
+  let(:page_title) { 'Home' }
+
+  it_should_behave_like "all static pages"
+end
+end
+
+
