a1297823a3b34e06bcc8aad78d2192f1e2adc8ab
Rehana Khan <rehanakhan00@gmail.com>
2013-03-10 19:18:44 -0400

    Chapter 5 Exercise 3

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,46 @@
+
 require 'spec_helper'
+
 describe ApplicationHelper do
-describe "full_title" do
-it "should include the page title" do
-full_title("foo").should =~ /foo/
-end
-it "should include the base title" do
-full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
-end
-it "should not include a bar for the home page" do
-full_title("").should_not =~ /\|/
-end
-end
+
+    describe "full_title" do
+
+        it "should include the page title" do
+        full_title("foo").should =~ /foo/
+        end
+
+        it "should include the base title" do
+        full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
+        end
+
+        it "should not include a bar for the home page" do
+        full_title("").should_not =~ /\|/
+        end
+
+    end
+
+    it "should have the right links on the layout" do
+
+        visit root_path
+
+        click_link "About"
+        page.should have_selector 'title', :content => full_title('About Us')
+
+        click_link "Help"
+        page.should have_selector 'title', :content => full_title('Help')
+
+        click_link "Contact"
+        page.should have_selector 'title', :content => full_title('Contact')
+
+        click_link "Home"
+
+        click_link "Sign up now!"
+        page.should have_selector 'title', :content => full_title('Sign up')
+
+        click_link "sample app"
+        page.should have_selector 'title', :content => full_title('Sample app')
+
+    end
+
 end
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,6 +42,7 @@ describe "Static pages" do
 end
 =end
 
+=begin
 require 'spec_helper'
 
 describe "Static pages" do
@@ -104,6 +105,51 @@ describe "Static pages" do
         page.should have_selector 'title', :content => full_title('Sample app')
 
     end
+
+end
+=end
+
+require 'spec_helper'
+
+describe "Static pages" do
+
+    subject { page }
+
+    shared_examples_for "all static pages" do
+        it { should have_selector("h1", :content => heading) }
+        it { should have_selector("title", :content => full_title(page_title)) }
+    end
+
+    describe "Home page" do
+        before { visit root_path }
+        let(:heading) { 'Sample App' }
+        let(:page_title) { '' }
+        it_should_behave_like "all static pages"
+        it { should_not have_selector 'title', text: '| Home' }
+    end
+
+    describe "Help page" do
+        before { visit root_path }
+        let(:heading) { "Help" }
+        let(:page_title) { "Help" }
+        it_should_behave_like "all static pages"
+    end
+
+    describe "About page" do
+        before { visit root_path }
+        let(:heading) { "About" }
+        let(:page_title) { "About Us" }
+        it_should_behave_like "all static pages"
+    end
+
+    describe "Contact page" do
+        before { visit root_path }
+        let(:heading) { "Contact" }
+        let(:page_title) { "Contact" }
+        it_should_behave_like "all static pages"
+    end
+
+    include ApplicationHelper
 
 end
 
