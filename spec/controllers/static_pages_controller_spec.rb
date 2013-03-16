ab7c97adfab1f35dd61ea9e3d160c188029b16f5
Rehana Khan <rehanakhan00@gmail.com>
2013-03-15 23:53:57 -0400

    Make a basic User model(including secure passwords)

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,4 +1,5 @@
-#require 'spec_helper'
+require 'spec_helper'
+
 #describe StaticPagesController do
 #  describe "GET '...'" do
 #    it "returns http success" do
@@ -9,24 +10,23 @@
 #end
 #end
 
-
-
+=begin
 describe "Static pages" do
 
 subject { page }
 
-shared_examples_for "all static pages" do
-  it { should have_selector('h1',    text: heading) }
-  it { should have_selector('title', text: full_title(page_title)) }
+shared_examples_for "foo bar pages" do
+  it { should have_selector("h1",    :text => heading) }
+  it { should have_selector("title", :text => full_title(page_title)) }
 end
 
 describe "Home page" do
   before { visit root_path }
-  let(:heading)    { 'Sample App' }
-  let(:page_title) { 'Home' }
+  let(:heading)    { "Sample App" }
+  let(:page_title) { "Home" }
 
-  it_should_behave_like "all static pages"
+  it_should_behave_like "foo bar pages"
 end
 end
-
+=end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,16 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
+require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
- config.include Rails.application.routes.url_helpers  
+    config.include Rails.application.routes.url_helpers  
+    config.include Capybara::DSL
 
 # ## Mock Framework
   #
