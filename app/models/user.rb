d0bc4a0f05e8d4adea9a3aef0891510ed1b0aba2
Rehana Khan <rehanakhan00@gmail.com>
2013-03-23 00:33:09 -0400

    Exercises-7.6

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@
 # updated_at :datetime not null
 #
 
+=begin
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
@@ -21,3 +22,18 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 end
+=end
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+end
+
